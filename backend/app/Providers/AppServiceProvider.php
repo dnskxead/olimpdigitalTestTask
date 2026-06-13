240a47cec220d38e21cf2e8fa6d9c9714233ee1c
Denis Hrytsai <dnskxead@gmail.com>
2026-06-13 04:46:21 +0000

    changed AppServiceProvider and method register inside

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,20 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\CarAvailabilityServiceInterface;
+use App\Services\CarAvailabilityService;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        // Біндимо інтерфейс до конкретної реалізації
+        $this->app->bind(CarAvailabilityServiceInterface::class, CarAvailabilityService::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
