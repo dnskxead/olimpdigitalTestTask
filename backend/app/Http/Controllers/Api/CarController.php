7b7640f393f385b6ac00f818b29dd5077da76f58
Denis Hrytsai <dnskxead@gmail.com>
2026-06-13 05:04:30 +0000

    changed CarController and updated method function __construct

--- backend/app/Http/Controllers/Api/CarController.php
+++ backend/app/Http/Controllers/Api/CarController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
+use App\Http\Requests\CarAvailabilityRequest;
 use App\Interfaces\CarAvailabilityServiceInterface;
+use Illuminate\Http\JsonResponse;
 
 class CarController extends Controller
 {
     protected CarAvailabilityServiceInterface $availabilityService;
+
     public function __construct(CarAvailabilityServiceInterface $availabilityService)
     {
         $this->availabilityService = $availabilityService;
     }
 
-    public function index(Request $request): JsonResponse
+    public function index(CarAvailabilityRequest $request): JsonResponse
     {
-        $startDate = $request->query('start_date', '2023-01-01 00:00:00');
-        $endDate = $request->query('end_date', '2023-01-31 23:59:59');
-        $data = $this->availabilityService->getAvailability($startDate, $endDate);
+        $data = $this->availabilityService->getAvailability(
+            $request->validated('start_date'),
+            $request->validated('end_date')
+        );
+
         return response()->json([
             'success' => true,
             'data' => $data
